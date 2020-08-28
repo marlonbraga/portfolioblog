2be097a230ff8a14c46a34f3f1ab135e75949f5d
Marlon Braga <marlon@marlonbraga.com.br>
2020-08-28 16:01:35 -0300

    Scaffolding site pages
    Create Bootstrap containers and general website style

--- marlonbraga.dev/Controllers/HomeController.cs
+++ marlonbraga.dev/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace marlonbraga.dev.Controllers {
 			return View();
 		}
 
+		public IActionResult Sobre() {
+			return View();
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error() {
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
