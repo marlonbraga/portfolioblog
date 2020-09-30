4796cf5a6fe1b4bed7e544b5c193e35acb752b16
Marlon Braga <marlon@marlonbraga.com.br>
2020-09-30 17:30:08 -0300

    Render Html on Details page
    Ajust Edit page
    
    Show Posts on Home Page

--- marlonbraga.dev/Controllers/HomeController.cs
+++ marlonbraga.dev/Controllers/HomeController.cs
@@ -6,17 +6,21 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using marlonbraga.dev.Models;
+using marlonbraga.dev.Models.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace marlonbraga.dev.Controllers {
 	public class HomeController:Controller {
+		private readonly Context _context;
 		private readonly ILogger<HomeController> _logger;
 
-		public HomeController(ILogger<HomeController> logger) {
+		public HomeController(ILogger<HomeController> logger, Context context) {
 			_logger = logger;
+			_context = context;
 		}
 
-		public IActionResult Index() {
-			return View();
+		public async Task<IActionResult> IndexAsync() {
+			return View(await _context.Posts.ToListAsync());
 		}
 
 		public IActionResult Privacy() {
